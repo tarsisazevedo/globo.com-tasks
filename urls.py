08a02bf55e084189cf3ab6a99609067cce68591d
Tarsis Figueredo Azevedo <tarsis.azevedo@gmail.com>
2010-12-29 22:42:05 -0200

    Adcionar url para index

--- urls.py
+++ urls.py
@@ -3,12 +3,14 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+from views import index
+
 urlpatterns = patterns('',
     # Example:
     # (r'^meu_delicious/', include('meu_delicious.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    # (r'^admin/docs/', include('django.contrib.admindocs.urls')),
+    (r'^$', index),
     (r'^admin/', include(admin.site.urls)),
 )
