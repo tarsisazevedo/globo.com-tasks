6f1db4be1b1a2dd7d865e68cd25168732c881319
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2010-12-30 08:15:37 -0200

    Melhorar layout para melhor visialização dos links

--- urls.py
+++ urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 from django.contrib import admin
-admin.autodiscover()
 
 from views import index
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^meu_delicious/', include('meu_delicious.foo.urls')),
+admin.autodiscover()
 
+urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/docs/', include('django.contrib.admindocs.urls')),
     (r'^$', index),
     (r'^admin/', include(admin.site.urls)),
+    (r'^media/(.*)$', 'django.views.static.serve',
+           {'document_root': settings.MEDIA_ROOT}),
 )
--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 from favoritos.models import Favorito
 
 def index(request):
     favoritos = Favorito.objects.all()
-    return render_to_response('index.html', locals())
+    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
