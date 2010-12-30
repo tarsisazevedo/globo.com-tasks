b7b496e804ace0b89992c033366f3213200380b2
Tarsis Figueredo Azevedo <tarsis.azevedo@gmail.com>
2010-12-29 23:02:58 -0200

    Adicionar tests de rota.

--- favoritos/tests.py
+++ favoritos/tests.py
@@ -1,23 +1,10 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+import unittest
+from should_dsl import should
 
-Replace these with more appropriate tests for your application.
-"""
+from django.test import Client
 
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+class TesteGeral(unittest.TestCase):
+    def test_deve_estar_disponivel_url_index(self):
+        client = Client()
+        response = client.post('/')
+        response.status_code |should| equal_to(200)
