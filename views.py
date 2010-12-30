8c7cbc339d4a061d1f7409b1c196c777e4b1ddb5
Tarsis Figueredo Azevedo <tarsis.azevedo@gmail.com>
2010-12-29 22:57:00 -0200

    Mudar views para carregar todas os favoritos cadastrados

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from favoritos.models import Favorito
 
 def index(request):
-    return render_to_response('index.html')
+    favoritos = Favorito.objects.all()
+    return render_to_response('index.html', locals())
