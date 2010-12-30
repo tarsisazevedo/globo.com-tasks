dd733dfd486f07eb6d28109581ee0a23ac63cc61
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2010-12-30 10:40:18 -0200

    Adicionar funcionalidade de login

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 from views import index
 
@@ -10,6 +11,9 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/docs/', include('django.contrib.admindocs.urls')),
     (r'^$', index),
+    (r'^login/$', direct_to_template, {"template": 'login.html'}),
+    (r'^accounts/login/$', 'views.meu_delicious_login'),
+	(r'^accounts/logout/$', 'views.meu_delicious_logout'),
     (r'^admin/', include(admin.site.urls)),
     (r'^media/(.*)$', 'django.views.static.serve',
            {'document_root': settings.MEDIA_ROOT}),
--- views.py
+++ views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 
 from favoritos.models import Favorito
 
 def index(request):
     favoritos = Favorito.objects.all()
     return render_to_response('index.html', locals(), context_instance=RequestContext(request))
+
+def meu_delicious_login(request):
+    try:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password) 
+        if user.is_active:
+            login(request, user)
+            return HttpResponseRedirect(request.META["HTTP_REFERER"])
+    except:
+        return HttpResponseRedirect('/')
+
+def meu_delicious_logout(request):
+    logout(request)
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
