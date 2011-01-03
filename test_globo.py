c2323df9d780e8d7fbc53d1fe9a74b32cf9b80f7
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-01-03 16:26:31 -0200

    Remover else desnecessario.

--- test_globo.py
+++ test_globo.py
@@ -12,12 +12,11 @@ def numero_minimo(maximo_divisor):
     numero_base = 0
     while flag:
         numero_base += 1
-        for i in range(1, maximo_divisor+1):
-            if numero_base % i != 0:
+        for divisor in range(1, maximo_divisor+1):
+            if numero_base % divisor != 0:
                 break
-            else:
-                if i == maximo_divisor:
-                    flag = False
+            elif divisor == maximo_divisor:
+                flag = False
 
     return numero_base    
 
