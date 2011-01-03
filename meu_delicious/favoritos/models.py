a31d161c0c363efe8f302b4051759f8d01bfefe2
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-01-03 12:51:27 -0200

    Modificar models para ordenar por data e criar o formulario.

--- meu_delicious/favoritos/models.py
+++ meu_delicious/favoritos/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
 
 import datetime
 
 class Favorito(models.Model):
     titulo = models.CharField(max_length=200)
-    url = models.URLField()
-    data = models.DateField(default=datetime.datetime.now())
+    url = models.URLField(verify_exists=True)
+    data = models.DateTimeField(default=datetime.datetime.now(), editable=False)
     user = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ('-data',)
 
     def __unicode__(self):
         return self.titulo
+
+class FavoritoForm(forms.ModelForm):
+    class Meta:
+        model = Favorito
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        return url.strip()
