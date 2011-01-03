9dd44cc58a53b3c16518a1b35c1088301faa3d92
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-01-03 16:59:54 -0200

    Remover arquivos antigos

--- meu_delicious/favoritos/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from favoritos.models import Favorito
-
-admin.site.register(Favorito)
--- meu_delicious/favoritos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic.create_update import create_object
-
-from favoritos.views import index, novo_favorito
-
-urlpatterns = patterns('',
-    (r'^$', index),
-    (r'^novo_favorito/$', novo_favorito),
-)
